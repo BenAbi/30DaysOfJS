77925e6292d26c4c2861a430f53d51d33edaa2aa
Ben Abi <abrahamdesantaclara@gmail.com>
2022-07-04 09:26:15 +0200

    array

--- 02-Day/main.js
+++ 02-Day/main.js
@@ -24,7 +24,54 @@ let indefinido = undefined;
  *  Matrices
  */
 
+//  Array
 let numeros = [1, 2, 3];
 numeros[0] = 0;
 
 console.log(numeros);
+
+let user01 = {
+    firstName: 'Manolo',
+    lastName: 'Ortiz',
+    numberID: '001',
+    city: 'Madrid',
+    job: 'teacher'
+}
+
+let user02 = {
+    firstName: 'Pedro',
+    lastName: 'Lee',
+    numberID: '342',
+    city: 'Barcelona',
+    job: 'doctor'
+}
+
+let user03 = {
+    firstName: 'Pedro',
+    lastName: 'Lee',
+    numberID: '342',
+    city: 'Barcelona',
+    job: 'doctor'
+}
+
+let user04 = user03;
+
+console.log(user01);
+console.log(user02, user03, user04);
+
+console.log(user01 == user02); //false
+console.log(user02 == user03); //false
+console.log(user01 == user01); //true
+console.log(user03 == user04); //true
+console.log(user02 == user04); //false
+
+
+user04.firstName = 'Martha'; //cambia el dato [firstName] de user04 y por ende del user03
+user03[0] = 'Sandra';
+console.log(user04[0]); //agrega un dato llamado 0 en el objeto user
+console.log(typeof user04.firstName)
+console.log(typeof user04)
+console.log(user04, user03);
+console.log(user03 == user04); //true user03 y user04 son el mismo
+console.log(user02 == user04);
+
