a1fe8971586f65479482d6aa2d3bf1de0f0c6334
Ben Abi <abrahamdesantaclara@gmail.com>
2022-07-04 07:19:22 +0200

    02 day

--- 02-Day/main.js
+++ 02-Day/main.js
@@ -1,6 +1,30 @@
 // Tipos de datos
+console.log('02 Day: Data types');
 
-// Datos primitivos
+// 
+/** Datos primitivos
+ *  Numeros: enteros y flotantes
+ *  Cadenas: cualquier dato contenido entre " "; ' '
+ *  Booleanos: true or false
+ *  Nulo: null
+ *  Indefinido: undefined 
+ */
+
+let enteros = 25;
+let flotantes = 3.5;
+let plabara = 'Hola';
+let verdadero = true;
+let datoVacio = null;
+let indefinido = undefined;
 
 
-// Datos no primitivos
+/** Datos no primitivos
+ *  Objetos
+ *  Funciones
+ *  Matrices
+ */
+
+let numeros = [1, 2, 3];
+numeros[0] = 0;
+
+console.log(numeros);
